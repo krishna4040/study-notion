ae48b7d8ac967636739bcf3e9aae10b6c2116f50
krishna jain <115697713+krishna4040@users.noreply.github.com>
2024-01-03 18:04:18 +0530

    express server setup

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,4 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+require('dotenv').config();
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.get('/', (req, res) => {
+    res.send('<h1>Home page</h1>');
+});
+app.listen(process.env.PORT, () => {
+    console.log("App is running succsesfully at", process.env.PORT);
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,13 @@
-import express, { Request, Response, Application } from 'express'
+import express, { Application, Request, Response } from 'express'
+require('dotenv').config();
 const app: Application = express();
 
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('<h1>Home page</h1>');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("App is running succsesfully at", process.env.PORT);
+});
