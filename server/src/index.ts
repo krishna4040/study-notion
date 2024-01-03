3d4643af464af508cc8ceeb18cfa9890dd5dfb7e
krishna jain <115697713+krishna4040@users.noreply.github.com>
2024-01-04 00:56:08 +0530

    server initialised

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,39 @@
 import express, { Application, Request, Response } from 'express'
 import expressFileUpload, { Options } from 'express-fileupload'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 require('dotenv').config();
-const app: Application & Options = express();
+import { dbConnect } from './config/dbConnect'
+import { cloudinaryConnect } from './config/cdConnect'
 
+import courseRouter from './router/course'
+import paymentRouter from './router/payment'
+import profileRouter from './router/profile'
+import userRouter from './router/user'
+
+const app: Application & Options = express();
+dbConnect();
+cloudinaryConnect();
+
+app.use(
+    cors({
+        origin: '*',
+        credentials: true
+    })
+);
 app.use(express.json());
-app.use(expressFileUpload());
+app.use(expressFileUpload(
+    {
+        useTempFiles: true,
+        tempFileDir: '/temp'
+    }
+));
+app.use(cookieParser());
+
+app.use('/api/auth', userRouter);
+app.use('/api/profile', profileRouter);
+app.use('/api/payment', paymentRouter);
+app.use('/api/course', courseRouter);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('<h1>Home page</h1>');
