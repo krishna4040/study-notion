75d28f4a05fae08c2439bb644b3e676cb54183c7
krishna jain <115697713+krishna4040@users.noreply.github.com>
2024-01-16 23:39:11 +0530

    fixed the image path issue in timeline section

--- src/components/core/Home/TimeLineSection.tsx
+++ src/components/core/Home/TimeLineSection.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import logo1 from '@/assets/TimeLineLogo/Logo1.svg'
-import logo2 from '@/assets/TimeLineLogo/logo2.svg'
-import logo3 from '@/assets/TimeLineLogo/logo3.svg'
-import logo4 from '@/assets/TimeLineLogo/logo4.svg'
-import timeimg from '@/assets/Images/TimeLineImage.png'
+import logo2 from '@/assets/TimeLineLogo/Logo2.svg'
+import logo3 from '@/assets/TimeLineLogo/Logo3.svg'
+import logo4 from '@/assets/TimeLineLogo/Logo4.svg'
+import timeimg from '@/assets/Images/TimelineImage.png'
 import Image, { StaticImageData } from 'next/image'
 
 interface tl {
